7747979dd51a8e9f5ec2b40ef0b94645301ffaf0
Diogo L.O.C <locdiogo@gmail.com>
2023-04-14 17:59:54 -0300

    Changes....

--- to_be_implemented/finder_earthquakes/plotting_event_by_dailyspec.py
+++ to_be_implemented/finder_earthquakes/plotting_event_by_dailyspec.py
@@ -83,12 +83,14 @@ BINARY_FILES = '/home/diogoloc/dados_posdoc/ON_MAR/EARTHQUAKE_FINDER_NETWORK_OUT
 # Parameters
 # ==========
 
-FIRSTDAY = '2020,03,25,11,00,00'
-LASTDAY = '2020,03,25,11,59,00'
+EVENT_TIME = '2019,11,19,23,16,00'
+
+FIRSTDAY = '2019,11,19,22,45,00'
+LASTDAY = '2019,11,19,23,45,00'
 
 NETWORK = 'ON'
 
-STATION = 'OBS18'
+STATION = 'OBS22'
 
 CHANNEL = 'HHZ'
 
@@ -160,68 +162,20 @@ def filelist(basedir,interval_period_date):
     return sorted(files)
 
 # ------------------------------------------------------------------------------
-
-def create_axes(ratio_LF_height=0.6, figsize=figsize):
-    """
-    Create necessary axis objects
-    :param ratio_LF_height: float
-        Ratio of LF spectrograms height to total spectrogram height
-    :param figsize:  tuple
-        Figure size in inches
-    :return: matplotlib.Axis
-    """
-    fig = plt.figure(figsize=figsize)
-
-    # [left bottom width height]
-    h_spec_total = 0.7
-    h_base = 0.13
-
-    h_spec_LF = h_spec_total * ratio_LF_height
-    h_spec_HF = h_spec_total - h_spec_LF
-    h_seis = 0.15  # 0.2
-    w_base = 0.08
-    w_spec = 0.77
-    w_psd = 0.1
-    ax_seis_LF = fig.add_axes([w_base, h_base + h_spec_total,
-                               w_spec, h_seis],
-                              label='seismogram LF')
-    ax_seis_HF = ax_seis_LF.twinx()
-    ax_spec_LF = fig.add_axes([w_base, h_base, w_spec, h_spec_LF],
-                              sharex=ax_seis_LF,
-                              label='spectrogram LF')
-    ax_spec_HF = fig.add_axes([w_base, h_base + h_spec_LF, w_spec, h_spec_HF],
-                              sharex=ax_seis_LF,
-                              label='spectrogram HF')
-    ax_psd_LF = fig.add_axes([w_base + w_spec, h_base, w_psd, h_spec_LF],
-                             sharey=ax_spec_LF,
-                             label='PSD LF')
-    ax_psd_HF = fig.add_axes([w_base + w_spec, h_base + h_spec_LF,
-                              0.1, h_spec_HF],
-                             sharey=ax_spec_HF,
-                             label='PSD HF')
-    # Colorbar axis
-    ax_cb = fig.add_axes([w_base + w_spec + w_psd / 2.,
-                          h_base + h_spec_HF + h_spec_LF + h_seis * 0.1,
-                          w_psd * 0.2,
-                          h_seis * 0.8],
-                         label='colorbar')
-    return fig, ax_cb, ax_psd_HF, ax_psd_LF, ax_seis_HF, ax_seis_LF,ax_spec_HF, ax_spec_LF
-
 def format_axes(ax_cb: plt.Axes,
                 ax_psd_HF: plt.Axes, ax_psd_LF: plt.Axes, ax_seis_HF: plt.Axes, ax_seis_LF: plt.Axes,
                 ax_spec_HF: plt.Axes, ax_spec_LF: plt.Axes, fmax_HF: plt.Axes,
                 fmax_LF: float, fmin_HF: float, fmin_LF: float, st_HF: Stream, st_LF: Stream,
                 tstart: datetime, tend: datetime, dBmax: float
                 ):
-    ax_seis_LF.set_ylabel('%s \n[µm/s²]' % st_LF[0].get_id(), color='grey')
-    ax_seis_HF.set_ylabel('%s \n[µm/s] > 1 Hz' % st_HF[0].get_id(),color='k')
+    ax_seis_LF.set_ylabel('%s \n[µm/s²] < 1 Hz' % st_LF[0].get_id(), color='grey')
+    ax_seis_HF.set_ylabel('%s \n[µm/s²] > 1 Hz' % st_HF[0].get_id(),color='k')
 
     ax_seis_LF.tick_params('y', colors='grey')
     ax_seis_HF.tick_params('y', colors='k')
     ax_spec_HF.set_ylim(fmin_HF, fmax_HF)
-    ax_spec_HF.set_ylabel('Frequency [Hz]', fontsize=12)
-    ax_spec_LF.set_ylabel('Period [s]', fontsize=12)
-    ax_spec_LF.set_yscale('log')
+    ax_spec_HF.set_ylabel('Frequência [Hz]', fontsize=12)
+    ax_spec_LF.set_ylabel('Frequência [Hz]', fontsize=12)
 
     for ax_psd in [ax_psd_HF, ax_psd_LF]:
         ax_psd.yaxis.set_label_position("right")
@@ -229,8 +183,15 @@ def format_axes(ax_cb: plt.Axes,
     ax_psd_LF.set_ylim(fmin_LF, fmax_LF)
 
     ax_psd_LF.set_xlabel('PSD [dB]')
-    ax_psd_LF.set_ylabel('Period [s]', fontsize=12, rotation=270., va='bottom')
-    ax_psd_HF.set_ylabel('Frequency [Hz]', fontsize=12, rotation=270., va='bottom')
+    ax_psd_LF.set_ylabel('Frequência [Hz]', fontsize=12, rotation=270., va='bottom')
+    ax_psd_HF.set_ylabel('Frequência [Hz]', fontsize=12, rotation=270., va='bottom')
+    ax_psd_HF.yaxis.set_major_locator(MultipleLocator(10))
+    ax_psd_HF.yaxis.set_minor_locator(MultipleLocator(1))
+    ax_psd_LF.yaxis.set_major_locator(MultipleLocator(0.2))
+    ax_psd_LF.yaxis.set_minor_locator(MultipleLocator(0.1))
+    ax_psd_LF.xaxis.set_major_locator(MultipleLocator(50))
+    ax_psd_LF.xaxis.set_minor_locator(MultipleLocator(10))
+
     ax_psd_HF.set_xticks([])
     # make unnecessary labels disappear
     for ax in [ax_spec_HF, ax_seis_LF]:
@@ -253,12 +214,12 @@ def format_axes(ax_cb: plt.Axes,
     ax_spec_HF.xaxis.offsetText.get_text()
     ax_spec_HF.xaxis.offsetText.set_text('')
     ax_spec_HF.xaxis.offsetText.set_visible(False)
-    hours = mdates.MinuteLocator(interval=5)
-    mins = mdates.MinuteLocator(interval=1)
+    mins5 = mdates.MinuteLocator(interval=5)
+    mins1 = mdates.MinuteLocator(interval=1)
     # format the ticks
     for ax in (ax_spec_HF, ax_spec_LF):
-        ax.xaxis.set_major_locator(hours)
-        ax.xaxis.set_minor_locator(mins)
+        ax.xaxis.set_major_locator(mins5)
+        ax.xaxis.set_minor_locator(mins1)
 
 
 # =================
@@ -267,6 +228,7 @@ def format_axes(ax_cb: plt.Axes,
 
 fday = UTCDateTime(FIRSTDAY)
 lday = UTCDateTime(LASTDAY)
+eday = UTCDateTime(EVENT_TIME)
 
 tstart = fday.datetime
 tend = lday.datetime
@@ -381,7 +343,7 @@ ax_psd_LF = fig.add_axes([w_base + w_spec, h_base, w_psd, h_spec_LF], sharey=ax_
 ax_psd_HF = fig.add_axes([w_base + w_spec, h_base + h_spec_LF,0.1, h_spec_HF],sharey=ax_spec_HF,label='PSD HF')
 
 # Colorbar axis
-ax_cb = fig.add_axes([w_base + w_spec + w_psd / 2.,h_base + h_spec_HF + h_spec_LF + h_seis * 0.1, w_psd * 0.2, h_seis * 0.8], label='colorbar')
+ax_cb = fig.add_axes([w_base + w_spec + w_psd / 1.5,h_base + h_spec_HF + h_spec_LF + h_seis * 0.1, w_psd * 0.1, h_seis * 0.8], label='colorbar')
 
 
 for tr in st_LF:
@@ -393,6 +355,8 @@ for tr in st_HF:
     t_HF = np.arange(UTCDateTime(tr.stats.starttime).datetime,UTCDateTime(tr.stats.endtime + tr.stats.delta).datetime,datetime.timedelta(seconds=tr.stats.delta))
     ax_seis_HF.plot(t_HF, tr.data * 1e6,'k', lw=0.5,alpha=0.7, label='> 1 Hz')
     ax_seis_HF.legend(loc='lower right', fontsize=9, edgecolor='k', labelcolor='k')
+    ax_seis_HF.annotate(eday.strftime('%H:%M:%S'), (eday, tr.data.max()),xytext=(0.5, 0.2), textcoords='axes fraction',
+            arrowprops=dict(facecolor='r', shrink=0.005,alpha=0.75),fontsize=12,horizontalalignment='right', verticalalignment='top')
 
     
 for st, ax_spec, ax_psd, flim, winlen in zip([st_LF, st_HF],[ax_spec_LF, ax_spec_HF],[ax_psd_LF, ax_psd_HF],[(fmin_LF, fmax_LF),(fmin_HF, fmax_HF)], [winlen_LF, winlen_HF]):
@@ -401,29 +365,32 @@ for st, ax_spec, ax_psd, flim, winlen in zip([st_LF, st_HF],[ax_spec_LF, ax_spec
         p, f, t = mlab.specgram(tr.data, NFFT=winlen,Fs=tr.stats.sampling_rate,noverlap=int(winlen * overlap),pad_to=next_pow_2(winlen) * 4)
 
         t = np.array([UTCDateTime(i + tr.stats.starttime.timestamp) for i in t])
-  
+    
         delta = t[1] - t[0]
         t = np.arange(t[0].datetime,(t[-1] + delta * 0.9).datetime,datetime.timedelta(seconds=delta))
 
         bol = np.array((f >= flim[0] * 0.9, f <= flim[1])).all(axis=0)
-        
+            
         vmin = np.percentile(10 * np.log10(p[bol, :]), q=1, axis=None)
         vmax = np.percentile(10 * np.log10(p[bol, :]), q=90, axis=None)
-
+        
         ax_spec.pcolormesh(t, f[bol], 10 * np.log10(p[bol, :]),vmin=dBmin,vmax=dBmax, shading='nearest',cmap='inferno')
 
         median = np.percentile(p[bol, :], axis=1, q=50)
         perc_95 = np.percentile(p[bol, :], axis=1, q=95)
         perc_01 = np.percentile(p[bol, :], axis=1, q=1)
-        ax_psd.plot(10 * np.log10(median), f[bol],color='k',lw=0.25)
-        ax_psd.plot(10 * np.log10(perc_95), f[bol],color='r', linestyle='dashed',lw=0.25)
-        ax_psd.plot(10 * np.log10(perc_01), f[bol],color='darkgrey', linestyle='dotted',lw=0.25)
+        ax_psd.plot(10 * np.log10(median), f[bol],color='k',lw=0.3,label='Med.')
+        ax_psd.plot(10 * np.log10(perc_95), f[bol],color='r',lw=0.3,label='95%')
+        ax_psd.plot(10 * np.log10(perc_01), f[bol],color='darkgrey',lw=0.3,label='1%')
         nhnm = op.signal.spectral_estimation.get_nhnm()
         nlnm = op.signal.spectral_estimation.get_nlnm()
         ax_psd.fill_betweenx(y=1. / nlnm[0], x1=-300 * np.ones_like(nlnm[1]), x2=nlnm[1], color='lightgray')
         ax_psd.fill_betweenx(y=1. / nhnm[0], x2=300 * np.ones_like(nhnm[1]), x1=nhnm[1], color='lightgray')
         ax_psd.set_xlim(dBmin, dBmax)
 
-
+        if ax_psd == ax_psd_HF:
+            ax_psd.legend(fontsize='small',bbox_to_anchor=(0.45,0.975) , borderaxespad=0.,edgecolor='None',facecolor='None')
+        
 format_axes(ax_cb, ax_psd_HF, ax_psd_LF, ax_seis_HF, ax_seis_LF,ax_spec_HF, ax_spec_LF, fmax_HF,fmax_LF, fmin_HF, fmin_LF, st_HF, st_LF,tstart, tend, dBmax=vmax)
-plt.show()
+os.makedirs(EARTHQUAKE_FINDER_OUTPUT,exist_ok=True)
+plt.savefig(EARTHQUAKE_FINDER_OUTPUT+st_LF[0].get_id()+'.'+fday.strftime('.%Y.%m.%d.%H.%M.%S')+'.png')
